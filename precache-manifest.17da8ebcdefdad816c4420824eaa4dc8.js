be162556b5288c8923374e53693d07118f84001a
Filin <san.filinow2012@yandex.ru>
2020-08-15 23:38:02 +0500

    Updates

---
+++ precache-manifest.17da8ebcdefdad816c4420824eaa4dc8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "645e411afee162b9a1258cc4a1b8c140",
+    "url": "/green-data-test/index.html"
+  },
+  {
+    "revision": "4743cc9d490b8d95bfb5",
+    "url": "/green-data-test/static/css/2.25ff2e5b.chunk.css"
+  },
+  {
+    "revision": "d1363c4cab36a777345f",
+    "url": "/green-data-test/static/css/main.80f30573.chunk.css"
+  },
+  {
+    "revision": "4743cc9d490b8d95bfb5",
+    "url": "/green-data-test/static/js/2.98bee0a3.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/green-data-test/static/js/2.98bee0a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1363c4cab36a777345f",
+    "url": "/green-data-test/static/js/main.36982e37.chunk.js"
+  },
+  {
+    "revision": "10df88d723885e7dae9a",
+    "url": "/green-data-test/static/js/runtime-main.1b63a25a.js"
+  }
+]);
